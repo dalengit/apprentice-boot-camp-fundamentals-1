2a331d9d9dea45e8c53c4ea0123305679dade108
Dalen Chan <Dalen.Chan@brighthr.com>
2021-11-15 10:17:43 +0000

    Hello World!

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McrDigital.Bootcamp1.Cards
+{
+    class HelloWorld
+    {
+
+        public string HelloWorld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
