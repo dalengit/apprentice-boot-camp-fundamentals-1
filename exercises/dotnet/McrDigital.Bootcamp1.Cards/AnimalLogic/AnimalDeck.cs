9a0cb55dd66a63d3daea91ac38f9b2f352c2b6ad
Dalen Chan <Dalen.Chan@brighthr.com>
2021-11-16 11:41:58 +0000

    Spacing update

--- exercises/dotnet/McrDigital.Bootcamp1.Cards/AnimalLogic/AnimalDeck.cs
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/AnimalLogic/AnimalDeck.cs
@@ -1,45 +1,46 @@
+using System.Net.WebSockets;
+
 namespace McrDigital.Bootcamp1.Cards
 {
     using System;
     using System.Collections.Generic;
 
     public class AnimalDeck : IDeck
-
     {
-    private readonly List<AnimalCard> _cards;
+        private readonly List<AnimalCard> _cards;
 
-    public AnimalDeck()
-    {
-        _cards = new List<AnimalCard>();
-        foreach (Animal animal in Enum.GetValues(typeof(Animal)))
+        public AnimalDeck()
         {
-            _cards.Add(new AnimalCard(animal));
-            _cards.Add(new AnimalCard(animal));
-        }
-    }
-
-    public ICard Deal()
-    {
-        var card = _cards[0];
-        _cards.RemoveAt(0);
-        return card;
-    }
-
-    public List<ICard> GetCards()
-    {
-        var cards = new List<ICard>();
-        foreach (Animal animal in Enum.GetValues(typeof(Animal)))
-        {
-            cards.Add(new AnimalCard(animal));
-            cards.Add(new AnimalCard(animal));
+            _cards = new List<AnimalCard>();
+            foreach (Animal animal in Enum.GetValues(typeof(Animal)))
+            {
+                _cards.Add(new AnimalCard(animal));
+                _cards.Add(new AnimalCard(animal));
+            }
         }
 
-        return cards;
-    }
+        public ICard Deal()
+        {
+            var card = _cards[0];
+            _cards.RemoveAt(0);
+            return card;
+        }
 
-    public void Shuffle()
-    {
-        _cards.KnuthShuffle();
-    }
+        public List<ICard> GetCards()
+        {
+            var cards = new List<ICard>();
+            foreach (Animal animal in Enum.GetValues(typeof(Animal)))
+            {
+                cards.Add(new AnimalCard(animal));
+                cards.Add(new AnimalCard(animal));
+            }
+
+            return cards;
+        }
+
+        public void Shuffle()
+        {
+            _cards.KnuthShuffle();
+        }
     }
 }
